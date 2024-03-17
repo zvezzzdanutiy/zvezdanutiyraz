525d9c0a263e703f73e6e1013e3b1c8acf71d412
Vasuluy Solncev <iiialun10@inbox.ru>
2024-03-17 18:30:48 +0300

    n

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Button, Linking } from 'react-native';
 
-export default function App() {
+const Home = () => {
+  const handleTelegramButtonClick = () => {
+    const url = 'https://oauth.telegram.org/auth?bot_id=7122476551:AAGRldhloWEs-_jWsEkOTMZEsXhGE0dbXWQ&scope=write&nonce=12345678&origin=https://zvezdanutiyt.netlify.app/&request_access=write&return_to=https://t.me/Rjaka_prikol_bot';
+
+    Linking.openURL(url);
+  };
+
+  useEffect(() => {
+    const handleUrlChange = (event) => {
+      const { url } = event;
+      // Обработайте URL-адрес после авторизации
+      console.log(url);
+    };
+
+    Linking.getInitialURL()
+      .then((url) => {
+        if (url) {
+          // Обработайте первоначальный URL-адрес после авторизации
+          console.log(url);
+        }
+      })
+      .catch((error) => console.log(error));
+
+    Linking.addEventListener('url', handleUrlChange);
+
+    return () => {
+      Linking.removeEventListener('url', handleUrlChange);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Авторизация через Telegram" onPress={handleTelegramButtonClick} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Home;
